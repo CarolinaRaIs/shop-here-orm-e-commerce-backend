1e48d1f11940cdd5459096bfa8587706c3af404a
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-04-24 15:09:07 -0700

    Defined columns and their data types, validation rules, and associations with other models for the Product.js model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { validate } = require('../config/connection');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +11,49 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    
+    price: {
+      // DECIMAL(10,2) =  the maximum number of digits allowed is 10, with 2 digits reserved for the decimal part of the number
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      // isDecimal = (Sequalize validator) checks that the value is a decimal number
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // set a default of 10
+      defaultValue: 10,
+      // isNumeric = (Sequalize validator) checks that the value is numeric.
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      // category_id references the category model's id (references the id column in the category table)
+      // belongTo = create a foreign key reference in Product model to the Category model
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    }
+    
   },
   {
     sequelize,
@@ -19,5 +63,8 @@ Product.init(
     modelName: 'product',
   }
 );
+
+//Define the association between the Product and Category models
+Product.belongsTo(Category, { foreignKey: 'category_id'});
 
 module.exports = Product;
