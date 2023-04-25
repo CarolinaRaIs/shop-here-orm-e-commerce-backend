68078da9b75e71c86672604005c835f8b303ba35
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-04-25 16:09:52 -0700

    Added GET and DELETE routes to product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -2,21 +2,70 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
+// In insomnia: GET http://localhost:3002/api/product
+// category = modelName in Category.js model
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      //The Product model has associations with both the Category and Tag models
+      include: [
+        {
+          model:Category
+        },
+        {
+          model: Tag
+        }
+      ]
+    });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+// In insomnia: GET http://localhost:3002/api/product/:id
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productByID = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+
+      include: [
+        {
+          model: Category
+        },
+
+        {
+          model: Tag
+        }
+      ]
+    });
+
+    //If a product is found (truthy), it is sent back to the client in 
+    // JSON format with a 200 status code
+    // else, send a 404 status code and an error message
+    if (productByID) {
+      res.json(productByID);
+    } else {
+      res.status(404).json({err: "No product with this ID"});
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+// In insomnia: POST http://localhost:3002/api/product
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +97,8 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+// In insomnia: PUT http://localhost:3002/api/product/:id
+router.put('/:id', async(req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +139,30 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+// In insomnia: DELETE http://localhost:3002/api/product/:id
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    //If a deleted product is found (truthy), it is sent back to the client in 
+    // JSON format with a 200 status code
+    // else, send a 404 status code and an error message
+    if (deletedProduct) {
+      res.json(deletedProduct);
+    } else {
+      res.status(404).json({err: "No product with this ID"})
+    }
+
+  } catch (err) {
+    // 501 means "Not Implemented", indicating that the server  
+    // does not support the functionality required to fulfill the request
+    res.status(501).json(err);
+  }
 });
 
 module.exports = router;
